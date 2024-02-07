c88f717a78318c16ada56abc9ec50c079e10ea50
yaoyuhan <yaoyuhan0214@qq.com>
2024-02-07 17:45:11 +0900

    add annotation

--- barren_plateau.py
+++ barren_plateau.py
@@ -7,23 +7,76 @@ import pennylane as qml
 
 
 class BP:
+    """
+    This class is used to simulate the phenomenon of barren plateau.
+    """
+
     def __init__(self, modify: bool = False, qubits: List[int] = None, layers: List[int] = None,
-                 random_rotation_gate: List[str] = None, samples: int = 100, result: bool = False):
-        if qubits is None:
-            qubits = [2, 4, 6]
-        if layers is None:
-            layers = [5, 10, 20, 50, 100, 150, 200, 250, 300, 400, 500]
-        if random_rotation_gate is None:
-            random_rotation_gate = ['x', 'y', 'z']
+                 random_rotation_gate: List[str] = None, samples: int = 100, result: bool = False, save: bool = False):
+        """
+        Initializes a new instance of the Class.
+
+        Args:
+            param modify: A boolean flag indicating whether to use the modified circuit.
+            param qubits: A list of integers presents simulated qubits.
+            param layers: A list of integers presents simulated layers.
+            param random_rotation_gate: A list of 'x','y' and 'z'.
+            param samples: An integer presents the repetitions of the circuits.
+            param result: A boolean flag indicating whether to print the result.
+            param save: A boolean flag indicating whether to save the detail data.
+        """
+        try:
+            if not isinstance(modify, bool):
+                raise ValueError('param:{modify} must be a bool')
+            if qubits is None:
+                qubits = [2, 4, 6]
+                print('Test on {} qubits'.format(qubits))
+            elif not isinstance(qubits, list) or not all(isinstance(q, int) for q in qubits):
+                raise ValueError("param:{qubits} must be a list of integers")
+            if layers is None:
+                layers = [5, 10, 20, 50]
+                print('Test on {} layers'.format(layers))
+            elif not isinstance(layers, list) or not all(isinstance(_, int) for _ in layers):
+                raise ValueError("param:{layers} must be a list of integers")
+            if random_rotation_gate is None:
+                random_rotation_gate = ['x', 'y', 'z']
+            elif not isinstance(random_rotation_gate, list) or not all(
+                    isinstance(gate, str) for gate in random_rotation_gate):
+                raise ValueError("param:{random_rotation_gate} must be ['x', 'y', 'z']")
+            if not isinstance(samples, int) or samples <= 0:
+                raise ValueError("param:{samples} must be a positive integer")
+            if not isinstance(result, bool):
+                raise ValueError("param:{result} must be a bool")
+            if not isinstance(save, bool):
+                raise ValueError("param:{save} must be a bool")
+        except ValueError as e:
+            print('Error initial parameter:', e)
+            raise
+
         self.modify = modify
         self.qubits = qubits
         self.layers = layers
         self.random_rotation_gate = random_rotation_gate
         self.samples = samples
         self.result = result
-        self.detail_key = ['qubit', 'layer', 'paras', 'outputs', 'gradients', 'variance', 'modified']
+        self.save = save
 
     def rotation_gate(self, i: int, qubit: int, angle: float = None):
+        """
+        This function generates the rotation gate in each qubits and layers.
+
+        Args:
+            param i: An integer represents the current qubit.
+            param qubit: An integer represents the number of the qubits.
+            param angle: A float from 0 to 2π represents the rotation angle of the gate.
+
+        Self:
+            param modify, random_rotation_gate.
+
+        Return:
+            modified is True: CRX, CRY or CRZ.
+            modified is False: RX, RY or RZ.
+        """
         gate = np.random.choice(self.random_rotation_gate)
         if angle is None:
             angle = 2 * np.pi * np.random.rand()
@@ -43,6 +96,21 @@ class BP:
                 return qml.RZ(angle, wires=i)
 
     def RPQCs(self, qubit: int, layer: int, theta: float):
+        """
+        The main structure of the random parameterized quantum circuits(RPQCs).
+
+        Args:
+            param qubit: An integer presents simulated qubits.
+            param layer: An integer presents simulated layers.
+            param theta: A float from 0 to 2π represents the rotation angle for θ_{1,1}.
+
+        Self:
+            param modify.
+            func rotation_gate().
+
+        Return:
+            The measurement of this circuit.
+        """
         # initial layer
         if self.modify:
             for i in range(2 * qubit):
@@ -62,8 +130,20 @@ class BP:
         return qml.expval(qml.PauliZ(0) @ qml.PauliZ(1))
 
     def main(self):
-        detail = []
+        """
+        This is the main process of this class, which is used to generate data.
+
+        Self:
+            param modify, qubits, layers, samples, save.
+            func RPQCs(), save_detail_data().
+
+        Return:
+            param results: A list of lists, which contain the variance of the gradients.
+                len(result)=len(qubits) and len(result[0])=len(layers) .
+            param detail: A list of dictionary.
+        """
         results = []
+        detail = []
         for qubit in self.qubits:
             gradients_variance = []
             for layer in self.layers:
@@ -86,29 +166,58 @@ class BP:
                     outputs.append(circuit(para))
                     gradients.append(differential_circuit(para))
                 gradients_variance.append(np.var(gradients))
-                detail.append(
-                    {'qubit': qubit, 'layer': layer, 'paras': paras, 'outputs': outputs, 'gradients': gradients,
-                     'variance': gradients_variance[-1], 'modified': self.modify})
+                detail.append({'qubit': qubit, 'layer': layer, 'paras': paras, 'outputs': outputs,
+                              'gradients': gradients, 'variance': gradients_variance[-1], 'modified': self.modify})
+                if self.save:
+                    self.save_detail_data(detail[-1])
             results.append(gradients_variance)
-        return detail, results
-
-    def get_detail(self):
-        detail, _ = self.main()
-        if self.result:
-            print(detail)
-        return detail
+        return results, detail
 
     def get_results(self):
-        _, results = self.main()
+        """
+           Return the results. If result=True, then print the variance data.
+
+           Self:
+                param result.
+                func main().
+
+           Return:
+                param results: A list of lists, which contain the variance of the gradients.
+       """
+        results, _ = self.main()
         if self.result:
             print(results)
         return results
 
-    def save_detail_results(self):
-        detail, _ = self.main()
+    def get_detail(self):
+        """
+           Return the detail data. If result=True, then print the detail data.
+
+           Self:
+                param result.
+                func main().
+
+           Return:
+                param detail: A list of dictionary.
+       """
+        _, detail = self.main()
+        if self.result:
+            print(detail)
+        return detail
+
+    @staticmethod
+    def save_detail_data(detail: dict):
+        """
+        Save the detail data in detail_data.csv.
+
+        param detail: A list of dictionary. len(paras)=len(outputs)=len(gradients)=samples.
+                detail[0] = {'qubit': int, 'layer': int, 'paras': List[float], 'outputs': List[float],
+                'gradients': List[float], 'variance': float, 'modified': bool}.
+        """
+        detail_key = ['qubit', 'layer', 'paras', 'outputs', 'gradients', 'variance', 'modified']
         with open("detail_data.csv", "a", newline="") as file:
             file.write('\n')
-            detail_data = csv.DictWriter(file, fieldnames=self.detail_key)
+            detail_data = csv.DictWriter(file, fieldnames=detail_key)
             for row in detail:
                 row['paras'] = json.dumps(row['paras'])
                 row['outputs'] = json.dumps(row['outputs'])
