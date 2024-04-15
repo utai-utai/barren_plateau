a808c739a28e9306cea7dbe75a01ced165117c50
UTAI <1934332186@qq.com>
2024-04-15 19:52:30 +0900

    commit

--- barren/__init__.py
+++ barren/__init__.py
@@ -3,8 +3,8 @@ from .barren_plateau_multi import BPs
 from .function import bp, bps_gradient, bps_output
 from .plotting import PLOTTING
 from .setup_db import initialize_database
-from .experiment import ex1
+from .experiment import ex1, ex2_1, ex2_2, ex2_3, ex2_4, ex2, ex3
 
 version = 1.0
 
-__all__ = ['BP', 'BPs', 'PLOTTING', 'initialize_database', 'bp', 'bps_gradient', 'bps_output', 'ex1']
+__all__ = ['BP', 'BPs', 'PLOTTING', 'initialize_database', 'bp', 'bps_gradient', 'bps_output', 'ex1', 'ex2_1', 'ex2_2', 'ex2_3', 'ex2_4', 'ex2', 'ex3']
